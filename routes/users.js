9c6d2efee3a53b980dc238150fdb0e1ff3239af9
Hyra <stef@mindthecode.com>
2019-01-04 20:34:31 +0100

    Node and vue

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res) {
+  // res.send('respond with a resource');
+  res.json({
+    users: [
+      {
+        id: 1,
+        name: "Stef"
+      },
+      {
+        id: 2,
+        name: "Melvin"
+      }
+    ]
+  });
 });
 
 module.exports = router;
