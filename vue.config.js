2dc493db1258e19160ef6da898c5dc3f1dad372d
Hyra <stef@mindthecode.com>
2019-01-06 01:45:49 +0100

    Rogue log comment

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/no-commonjs, import/no-nodejs-modules */
 
 let target = "http://localhost:3000";
-// console.log(process.env);
+
 module.exports = {
   devServer: {
     proxy: {
