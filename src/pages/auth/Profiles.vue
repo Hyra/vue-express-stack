b48bf49e293f75f2d97d490fb2387017be0148c5
Hyra <stef@mindthecode.com>
2019-01-31 15:15:29 +0100

    Auto redirect to dojo if there's only 1 profile

--- src/pages/auth/Profiles.vue
+++ src/pages/auth/Profiles.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
   .profiles
-    h1 Profiles
+    p We found the following profiles for your account. Choose which one you want to access.
     ul
       li(v-for="profile in profiles")
         router-link(v-if="profile.isSensei" :to="{name: 'dashboard', params: {dojoSlug: profile.dojo.handle}}").
           {{ profile.dojo.title }}
         router-link(v-else :to="{name: 'student-dashboard', params: {dojoSlug: profile.dojo.handle}}").
           {{ profile.dojo.title }}
-    h2 ME
-    pre {{ me }}
-    h2 Profiles
-    pre {{ profiles }}
+    //- h2 ME
+    //- pre {{ me }}
+    //- h2 Profiles
+    //- pre {{ profiles }}
 </template>
 
 <script>
@@ -32,7 +32,24 @@ export default {
             }
           }
         }
-      `
+      `,
+      result(data) {
+        if (data.data.profiles.length === 1) {
+          const profile = data.data.profiles[0];
+
+          if (profile.isSensei) {
+            this.$router.push({
+              name: "dashboard",
+              params: { dojoSlug: profile.dojo.handle }
+            });
+          } else {
+            this.$router.push({
+              name: "student-dashboard",
+              params: { dojoSlug: profile.dojo.handle }
+            });
+          }
+        }
+      }
     },
     me: {
       query: gql`
