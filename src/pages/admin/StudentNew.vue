0cbd659c0526fedcdd233fd25d87b9b960af58d3
Hyra <stef@mindthecode.com>
2019-02-03 12:21:28 +0100

    New Student

--- src/pages/admin/StudentNew.vue
+++ src/pages/admin/StudentNew.vue
@@ -1,9 +1,69 @@
 <template>
-  <div class="students-new"><h1>New Student</h1></div>
+  <div class="students-new">
+    <h1>New Student</h1>
+    <el-form ref="verificationForm" label-width="120px">
+      <el-form-item label="E-mail">
+        <el-input placeholder="Please input" v-model="email"></el-input>
+      </el-form-item>
+      <el-form-item label="Firstname">
+        <el-input placeholder="Please input" v-model="firstname"></el-input>
+      </el-form-item>
+      <el-form-item label="Lastname">
+        <el-input placeholder="Please input" v-model="lastname"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="addStudent">Login</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
+import gql from "graphql-tag";
+
 export default {
-  name: "students-new"
+  name: "students-new",
+  data() {
+    return {
+      email: "a",
+      firstname: "a",
+      lastname: "a"
+    };
+  },
+  methods: {
+    addStudent() {
+      this.$apollo.mutate({
+        mutation: gql`
+          mutation(
+            $dojoSlug: String!
+            $email: String!
+            $firstname: String!
+            $lastname: String!
+          ) {
+            newStudent(
+              dojoSlug: $dojoSlug
+              email: $email
+              firstname: $firstname
+              lastname: $lastname
+            ) {
+              id
+              stripeId
+            }
+          }
+        `,
+        variables: {
+          dojoSlug: this.$route.params.dojoSlug,
+          email: this.email,
+          firstname: this.firstname,
+          lastname: this.lastname
+        },
+        update: (store, { data: { newStudent } }) => {
+          console.log(store);
+          console.log(newStudent);
+          this.$emit("myEvent");
+        }
+      });
+    }
+  }
 };
 </script>
