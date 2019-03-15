972bb7f05220fd04f47fe6194a7dbca90caafc8b
Hyra <stef@mindthecode.com>
2019-03-15 07:37:27 +0100

    Open up script

---
+++ openup.sh
@@ -0,0 +1,5 @@
+open http://localhost:3000/graphql
+open https://dashboard.stripe.com/acct_1EAxOUB7ItNPufgk/test/subscriptions
+open https://stripe-payments-demo.appspot.com/
+open http://localhost:8080/firewall/admin/students
+open https://app.buddy.works/mindthecode
