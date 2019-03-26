77acc55f0ad86c95bf124694d9dd18934c85c377
Hyra <stef@mindthecode.com>
2019-03-26 17:05:49 +0100

    Page fade transition

--- src/layouts/Admin.vue
+++ src/layouts/Admin.vue
@@ -84,12 +84,44 @@
               </ul>
             </div>
           </div>
-          <div class="container"><router-view /></div>
+          <div class="container">
+            <transition
+              name="fade"
+              mode="out-in"
+              @beforeLeave="beforeLeave"
+              @enter="enter"
+              @afterEnter="afterEnter"
+            >
+              <router-view
+            /></transition>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  name: "Admin",
+  methods: {
+    beforeLeave(element) {
+      this.prevHeight = getComputedStyle(element).height;
+    },
+    enter(element) {
+      const { height } = getComputedStyle(element);
+
+      element.style.height = this.prevHeight;
+
+      setTimeout(() => {
+        element.style.height = height;
+      });
+    },
+    afterEnter(element) {
+      element.style.height = "auto";
+    }
+  }
+};
+</script>
 
 <style lang="scss">
 header {
@@ -132,5 +164,18 @@ header {
       }
     }
   }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: height, opacity;
+  transition-timing-function: ease;
+  overflow: hidden;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
 </style>
